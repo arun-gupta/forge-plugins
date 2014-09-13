07499aca674696d152de4372d11dd27eaa4e3517
Arun Gupta <arun.gupta@gmail.com>
2014-09-13 17:53:01 +0200

    adding the first bigger TNT explosion

---
+++ src/org/devoxx4kids/forge/plugins/biggertnt/BiggerTNTExplosion.java
@@ -0,0 +1,52 @@
+package org.devoxx4kids.forge.plugins.biggertnt;
+
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.item.EntityTNTPrimed;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+
+@Mod(name = BiggerTNTExplosion.NAME, modid = BiggerTNTExplosion.MODID, version = BiggerTNTExplosion.VERSION)
+public class BiggerTNTExplosion
+{
+    public static final String MODID = "examplemod";
+    public static final String VERSION = "1.0";
+    public static final String NAME = "1.7.10";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+//    	world.createExplosion(exploderEntity, x, y, z, radius, spawnParticles);
+		// some example code
+    	System.out.println(NAME + " mod loaded");
+    	MinecraftForge.EVENT_BUS.register(this);
+//        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+    
+    @SubscribeEvent
+    public void init(EntityJoinWorldEvent event)
+    {
+    	if (!(event.entity instanceof EntityTNTPrimed))
+    		return;
+    	
+    	EntityItem splosion = new EntityItem(event.world,
+    			event.entity.posX,
+				event.entity.posY,
+				event.entity.posZ,
+				new ItemStack(Blocks.dirt,1,0));
+		event.world.spawnEntityInWorld(splosion);
+		event.world.createExplosion(event.entity,
+    			event.entity.posX,
+				event.entity.posY,
+				event.entity.posZ,
+				25.0F, true);
+		event.world.removeEntity(splosion);
+    }
+    
+}
