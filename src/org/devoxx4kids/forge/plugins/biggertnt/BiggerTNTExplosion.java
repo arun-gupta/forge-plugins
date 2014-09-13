0a02855521d1807b72f1fab39f9d1e505cc49e6f
Arun Gupta <arun.gupta@gmail.com>
2014-09-13 18:00:42 +0200

    cleaned up code

--- src/org/devoxx4kids/forge/plugins/biggertnt/BiggerTNTExplosion.java
+++ src/org/devoxx4kids/forge/plugins/biggertnt/BiggerTNTExplosion.java
@@ -15,18 +15,15 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 @Mod(name = BiggerTNTExplosion.NAME, modid = BiggerTNTExplosion.MODID, version = BiggerTNTExplosion.VERSION)
 public class BiggerTNTExplosion
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "biggertnt";
     public static final String VERSION = "1.0";
     public static final String NAME = "1.7.10";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-//    	world.createExplosion(exploderEntity, x, y, z, radius, spawnParticles);
-		// some example code
     	System.out.println(NAME + " mod loaded");
     	MinecraftForge.EVENT_BUS.register(this);
-//        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
     }
     
     @SubscribeEvent
@@ -35,18 +32,18 @@ public class BiggerTNTExplosion
     	if (!(event.entity instanceof EntityTNTPrimed))
     		return;
     	
-    	EntityItem splosion = new EntityItem(event.world,
-    			event.entity.posX,
-				event.entity.posY,
-				event.entity.posZ,
-				new ItemStack(Blocks.dirt,1,0));
-		event.world.spawnEntityInWorld(splosion);
+//    	EntityItem splosion = new EntityItem(event.world,
+//    			event.entity.posX,
+//				event.entity.posY,
+//				event.entity.posZ,
+//				new ItemStack(Blocks.dirt,1,0));
+//		event.world.spawnEntityInWorld(splosion);
 		event.world.createExplosion(event.entity,
     			event.entity.posX,
 				event.entity.posY,
 				event.entity.posZ,
 				25.0F, true);
-		event.world.removeEntity(splosion);
+//		event.world.removeEntity(splosion);
     }
     
 }
